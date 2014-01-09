aa1a1a00545c111381e1d89b30c2260d58a99312
George Gecewicz <george@momnt.co>
2014-01-09 16:25:50 -0500

    initial commit – starting the theme

---
+++ assets/js/functions.js
@@ -0,0 +1,7 @@
+/**
+ *  Public-facing JavaScript for the Web Industries theme.
+ *  Copyright (c) 2013 George Gecewicz
+ */
+;(function($) {
+    console.log( $ )
+})(window.jQuery);
---
+++ index.php
@@ -0,0 +1,154 @@
+<?php get_header(); ?>
+
+  <div class="container">
+
+    <div id="topLine">
+      <div id="toplineText">Precision Converting & Outsource Manufacturing</div>
+      <div id="portal">Customer Portal &gt;&gt;</div>
+      <div id="search">Search &gt;&gt;</div>
+    </div>
+
+    <div id="logoLine">
+      <img src="images/WI-Logo.gif" alt="Web Industries" width="271" height="90" align="left">
+      
+      <div id="mainNav">
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#" class="footer" >ABOUT</a></li>
+          <li><a href="#" class="footer">MARKETS</a></li>
+          <li><a href="#" class="footer">SERVICES</a></li>
+          <li><a href="#" class="footer">LOCATIONS</a></li>
+          <li><a href="#" class="footer">NEWS</a></li>
+          <li><a href="#" class="footer">EVENTS</a></li>
+          <li><a href="#" class="footer">CONTACT</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="hometopSlide">
+        <div id="homeSlider"><img src="images/slideshowph.jpg">
+        </div> 
+        <div id="homeSlideRight">
+          <p class="fsize2"> <span class="orange">YOU&US=</span><br>
+SUCCESS</p>
+<p>Flexible material precision converting and end-product contract manufacturing are our core businesses. From composites to plastics, nonwovens to specialty films, we have experience with a range of materials that few companies can match–and a knowledge base that few companies can equal.</p>
+<p><a href="#">Learn More &gt;&gt;</a> </p>
+      </div>
+    </div> 
+    
+<div id="home5Pix">
+        <div class="home5p1"><img src="images/home_toppic1.jpg" width="188" height="179"></div>
+        <div class="home5p1"><img src="images/home_toppic2.jpg" width="188" height="179"></div>
+        <div class="home5p1"><img src="images/home_toppic3.jpg" width="188" height="179"></div>
+        <div class="home5p1"><img src="images/home_toppic4.jpg" width="187" height="179"></div>
+        <div class="home5p1"><img src="images/home_toppic5.jpg" width="188" height="179"></div>
+    </div>
+    
+    <div id="homeMain">
+        <div id="mainLeftSide">
+            <div id="mainLeftText">
+                  <p class="fsize2 notopmargin"><span class="orange">Web Industries</span>
+                    <br>
+                    Acquires CAD Cut</p>
+                  <p class="notopmargin">Companies combine to offer a full range of composite formatting and kitting solutions</p>
+                  <p>Web Industries, Inc., a leader in custom manufacturing and development services for flexible materials, is pleased to announce the acquisition of CAD Cut, Inc., 
+                  the leading provider of fabric cutting and kitting services for the aerospace, medical, and industrial fabrics markets. </p>
+                  <p class="fsize1"><a href="#">Learn More &gt;&gt;</a></p>
+            </div>
+          <div id="mainBottLeft">
+            <div class="mainBLPic"><img src="images/Home_bottpic1.jpg" width="331" height="249"></div>
+            <div class="mainBLPic"><img src="images/Home_bottpic2.jpg" width="331" height="249"></div>
+          </div>
+          <div id="socialIcons">
+            <img src="images/Home_lkin.jpg" width="78" height="79" alt="Linkedin">
+            <img src="images/Home_gplus.jpg" width="78" height="79" alt="Google plus">
+            <img src="images/Home_fb.jpg" width="76" height="79" alt="Facebook">
+            <img src="images/Home_email.jpg" width="77" height="79" alt="Email">
+          </div>
+      </div>
+      <div id="mainRightSide"><p><span class="fsize2 blue">NEWS</span><br>
+Web Industries and Massachusetts Bay Community College Join For Unique Training Program <br>
+  <a href="#">HTML or PDF</a></p>
+
+<p>Donald Romine, President & CEO of Web Industries, appears in the Worcester Business Journal<br>
+  <a href="#">HTML or PDF</a></p>
+
+<p>Web Industries Adds Additional Thin Nonwoven Printing Capabilities
+Upgrades at Dallas location allow cost-effective, high-speed printing and slitting in one pass<br>
+<a href="#">HTML or PDF</a></p>
+
+<p><a href="#">More News >></a></a>
+<span class="orange">................................................................</span>
+<p><span class="fsize2 blue">EVENTS</span><br>
+  <a href="#">JEC America 2013</a><br>
+Oct. 2-4, 2013<br>
+Boston Convention Center<br>
+Boston, MA  l  Booth G19<br>
+<a href="#">Request a meeting during this event</a></p>
+
+<p><a href="#">SAMPE Tech 2013</a><br>
+Oct. 22-23, 2013<br>
+Century II Convention Center<br>
+Wichita, KS  l  Booth B4<br>
+<a href="#">Request a meeting during this event</a></p>
+
+<p><a href="#">IWCS</a><br>
+Nov. 10-13, 2013<br>
+Charlotte Convention Center<br>
+Charlotte, NC  l  Booth 401<br>
+<a href="#">Request a meeting during this event</a></p>
+
+<p><a href="#">More Events >></a></p></div>
+        <div id="bottomNav"><a href="#">Contact Us</a></div>    
+        <div id="footer">
+            <div class="footernav1"><a href="#" class="footer">Home</a><br>
+          News<br>Social<br>Events<br>CAD Cut</div>
+           
+            <div class="footernavs">
+              About<br>
+              Overview<br>
+              History<br>
+              Values<br>
+              Culture<br>
+              Precision Converting<br>
+              Leadership              <br>
+              Quality<br>
+              Sustainability
+</div>
+    <div class="footernavs">Markets<br>
+              Composites<br>
+              Medical<br>
+              Consumer Products<br>
+          Wire &amp; Cable<br>
+          Innovations
+          </div>
+          <div class="footernav4">Services<br>
+            Process Design<br>
+            Product Development<br>
+            Slitting<br>
+            Spooling&amp; Rewinding<br>
+            Kitting, Assembly &amp; Packaging<br>
+            Printing, Coating &amp; Laminating<br>
+            Laser &amp; Knife Cutting
+            <br>
+            Specialty Film Extrusion
+            <br>
+            Sheeting<br>
+            Material &amp; Vendor Management<br>
+            Multi-Layer Insulation Blanket
+            </div>
+<div class="footernavs">Locations<br>
+               Atlanta<br>
+               Boston<br>
+               Denton<br>
+         Fort Wayne<br>
+        Montpelier<br>
+        Stade<br>
+        Composites Dev. Ctr.
+ </div>
+        </div>
+    
+  </div>
+
+</div><!-- /.container -->
+<?php get_footer(); ?>
